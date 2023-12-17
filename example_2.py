91684d6520f0ff94cc3de1e8e86a237fc2f8aa6e
DigInG Jenkins <diging@asu.edu>
2023-12-17 16:26:55 -0500

    Update example_2.py

--- example_2.py
+++ example_2.py
@@ -1,24 +1,25 @@
-input = [[97, 96, 96], [99, 98, 99], [96, 96, 97], [100, 99, 100]] 
-low_temps = []
-low_count = 0
-high_count = 0
-for value in input:
+def example_2():
+    input = [[97, 96, 96], [99, 98, 99], [96, 96, 97], [100, 99, 100]] 
+    low_temps = []
+    low_count = 0
+    high_count = 0
+    for value in input:
+        
+        LOW_TEMP = 97
+        average = 0
+        if sum(value)/len(value) < LOW_TEMP: 
+            average = sum(value)/len(value) 
+            low_count += 1
+        if average > 99:
+            average = sum (value)/len (value) 
+            high_count += 1
+        average = sum(value)/len(value)
+        
+        if average > 97 and average < 99: 
+            continue
     
-    LOW_TEMP = 97
-    average = 0
-    if sum(value)/len(value) < LOW_TEMP: 
-        average = sum(value)/len(value) 
-        low_count += 1
-    if average > 99:
-        average = sum (value)/len (value) 
-        high_count += 1
-    average = sum(value)/len(value)
-    
-    if average > 97 and average < 99: 
-        continue
-
-    print("Average is " + str(average))
-    
-measurement_count = len(input)
-low_percent= low_count/measurement_count*100
-print("Low temperatures in {0}%.".format(low_percent))
+        print("Average is " + str(average))
+        
+    measurement_count = len(input)
+    low_percent= low_count/measurement_count*100
+    print("Low temperatures in {0}%.".format(low_percent))
