25b8ddeb47980fee3a1739148e38d3f854fb3fb5
DigInG Jenkins <diging@asu.edu>
2023-12-17 15:56:10 -0500

    Update temp_example_1.py

--- temp_example_1.py
+++ temp_example_1.py
@@ -1,7 +1,7 @@
 normal_low = 97 # normal human body temperature lower end
 temperature_measurements_1 = [98, 99, 97, 95.2, 100, 101.1]
 
-normal_temperature_1= [] 
+Normal_temperature_1= [] 
 high_temperatures_2=[]
 normal_high = 99 # normal human body temperature higher end
 low_temperatures_1 = [] 
@@ -15,7 +15,7 @@ for temperature in temperature_measurements:
     elif temperature > normal_high: 
         high_temperatures_1.append(temperature)
     else:
-        normal_temperature_1.append(temperature)
+        Normal_temperature_1.append(temperature)
         
 temperature_measurements_2 = [98.2, 97, 97.1, 98.8, 100.2, 99]
 for temperature in temperature_measurements_2:
