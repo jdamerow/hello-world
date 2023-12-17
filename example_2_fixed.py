1d92adec2ff15e180428b36ad29ea598bb680b3b
DigInG Jenkins <diging@asu.edu>
2023-12-17 15:39:26 -0500

    Create example_2_fixed.py

---
+++ example_2_fixed.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+measurements = [[97, 96, 96], [99, 98, 99], [96, 96, 97], [100, 99, 100]]
+LOW_TEMP = 97
+HIGH_TEMP = 99
+
+def get_average(temperatures):
+    return sum(temperatures)/len(temperatures)
+
+def count_average(average, counter):
+    if average HIGH_TEMP:
+        counter['high'] += 1
+        
+def calculate_percentage(count, total):
+    return count/total*100
+
+average_counter = Counter({'low': 0, 'high': 0})
+
+for temperatures in measurements:
+    average = get_average(temperatures) 
+    print("Average is " + str(average))
+    
+    if average > LOW_TEMP and average < HIGH_TEMP: 
+        continue
+        
+    count_average(average, average_counter)
+    
+measurement_count = len(measurements)
+
+print("Low temperatures in {0}%.".format(calculate_percentage(average_counter['low'], measurement_count)))
+print("High temperatures in {0}%.".format(calculate_percentage(average_counter['high'], measurement_count)))
