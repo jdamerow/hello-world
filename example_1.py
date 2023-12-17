db532f2aeec791a3cd9caf830e9b410093d4e26f
DigInG Jenkins <diging@asu.edu>
2023-12-17 16:24:38 -0500

    Update example_1.py

--- example_1.py
+++ example_1.py
@@ -1,14 +1,15 @@
-input = [98, 99, 97, 95., 100, 101.1]
-losValue = []
-norms = []
-highs = []
-
-for y in input:
-  if y < 97:
-        los.append(x)
-  elif x > 99:
-        highs.append(x)
-  else:
-        norms.append(x)
-
-print(norms)
+def my_func():
+  input = [98, 99, 97, 95., 100, 101.1]
+  losValue = []
+  norms = []
+  highs = []
+  
+  for y in input:
+    if y < 97:
+          los.append(x)
+    elif x > 99:
+          highs.append(x)
+    else:
+          norms.append(x)
+  
+  print(norms)
